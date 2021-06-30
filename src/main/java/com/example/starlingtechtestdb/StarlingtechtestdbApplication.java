39dfed9fc413d4d92edfaa4e1a5159cd4a1dc3e9
devbarot <devarsh.barot@live.co.uk>
2021-06-30 23:04:23 +0100

    Initial commit

---
+++ src/main/java/com/example/starlingtechtestdb/StarlingtechtestdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.starlingtechtestdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlingtechtestdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarlingtechtestdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/starlingtechtestdb/StarlingtechtestdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starlingtechtestdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlingtechtestdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
